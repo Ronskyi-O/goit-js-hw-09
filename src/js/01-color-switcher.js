5f99103e5b63c2b2171ee3703760e4689759ace8
Oleksandr Ronskyi <oleksandr.ronskyi@gmail.com>
2022-10-29 21:51:07 -0400

    Task 01 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+let getEl = selector => document.querySelector(selector)
 
+getEl('[data-start]').addEventListener('click', colorSwitcherStart)
+getEl('[data-stop]').addEventListener('click', colorSwitcherStop)
+
+let timerId = null
+
+function colorSwitcherStart() {
+    timerId = setInterval(() => {
+        const color = getRandomHexColor()
+        getEl('body').style.backgroundColor = `${color}`;
+    }, 1000);
+
+     makeBtnStartDisabled()
+}
+
+function colorSwitcherStop() { 
+    clearInterval(timerId)
+
+    makeBtnStartAvailable()
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function makeBtnStartDisabled() {
+    getEl('[data-start]').setAttribute('disabled', '');
+}
+
+function makeBtnStartAvailable() {
+    getEl('[data-start]').removeAttribute('disabled');
+}
