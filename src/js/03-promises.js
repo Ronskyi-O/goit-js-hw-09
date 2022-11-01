004378d1b8d61db63495abe3b2d0c43264261f99
Oleksandr Ronskyi <oleksandr.ronskyi@gmail.com>
2022-10-31 21:12:12 -0400

    Task 03 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -15,41 +15,28 @@ function handleSubmit(event) {
   const amount = Number(getEl('[name="amount"]').value)
   
 
-  setTimeout(() => {
-     for (let i = 0; i < amount; i++) {
-       setTimeout(() => {
-         const delay = firstdelay + step * i
-         createPromise(i + 1, delay)
-      }, step)
+    for (let i = 0; i < amount; i++) {
+       const delay = firstdelay + step * i
+      setTimeout(() => {
+        createPromise(i + 1, delay)
+      }, delay)
   }
-  }, delay)
 }
-
 
 function createPromise(position, delay) {
-  //   const promise = new Promise((resolve, reject) => {
-  //     setTimeout(() => {
-  //     const shouldResolve = Math.random() > 0.3;
-  
-  //     if (shouldResolve) {
-  //       resolve({position, delay})
-  //     } else {
-  //       reject({position, delay})
-  //     }
-  //     })
-  //   }
-  // )}
-  // console.log(position);
-  // console.log(delay);
-
+    const promise = new Promise((resolve, reject) => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({position, delay})
+      } else {
+        reject({position, delay})
+      }
+    }
+  )
+promise.then(({ position, delay }) => {
+            Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+            })
+          .catch(({ position, delay }) => {
+            Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+          });
 }
-
-
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
-
